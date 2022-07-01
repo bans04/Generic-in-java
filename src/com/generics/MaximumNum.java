ea4bf571e165d607f8aa614b9178fab95a1059b5
bans04 <bansodednyanadeo@gmail.com>
2022-07-01 23:07:50 +0530

    Given 3 String find the Maximum.

--- src/com/generics/MaximumNum.java
+++ src/com/generics/MaximumNum.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 /*
  * Given 3 Integers find the maximum
  * Given 3 Float find the maximum
+ * Given 3 String find the maximum
  * 
  * @ Dnyandeo Bansode
  * 
@@ -21,13 +22,25 @@ public class MaximumNum {
 		return max;
 	}
 	
-	public Float getMaxNumFloat(Float num1, Float num2, Float num3) {
+	public Float getMaximumFloat(Float num1, Float num2, Float num3) {
 		Float max = num1;
 		if(num2.compareTo(max) > 0 && num2.compareTo(num3) > 0) {
 			max = num2;
 			return max;
 		}else if(num3.compareTo(max) > 0) {
 			max = num3;
+			return max;
+		}
+		return max;
+	}
+	
+	public String getMaximumString(String str1, String str2, String str3) {
+		String max = str1;
+		if(str2.compareTo(max) > 0 && str2.compareTo(str3) > 0) {
+			max = str2;
+			return max;
+		}else if(str3.compareTo(max) > 0) {
+			max = str3;
 			return max;
 		}
 		return max;
@@ -54,15 +67,25 @@ public class MaximumNum {
 		System.out.print("Enter first Float Num:-> ");
 		Float num3 = scanner.nextFloat();
 		
-		System.out.println("Maximum num is:-> " + getMaxNumFloat(num1, num2, num3));
+		System.out.println("Maximum num is:-> " + getMaximumFloat(num1, num2, num3));
+	}
+	
+	public void findMaxString() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter first str1:-> ");
+		String str1 = scanner.next();
+		System.out.print("Enter first str2:-> ");
+		String str2 = scanner.next();
+		System.out.print("Enter first str3:-> ");
+		String str3 = scanner.next();
+		
+		System.out.println("Maximum String is:-> " + getMaximumString(str1, str2, str3));
 	}
 
 	public static void main(String[] args) {
 		MaximumNum maximum = new MaximumNum();
 		//maximum.findMaxInteger();
-		
-		maximum.findMaxFloat();
-		
-		
+		//maximum.findMaxFloat();
+		maximum.findMaxString();
 	}
 }
