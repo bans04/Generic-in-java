cd7bd9b0c377babae70e40c4da329a29be5b8a37
bans04 <bansodednyanadeo@gmail.com>
2022-07-01 21:57:06 +0530

    Ensure the test data has Max number in the Seconde position.

--- src/com/generics/MaximumNumTestCases.java
+++ src/com/generics/MaximumNumTestCases.java
@@ -2,14 +2,28 @@ package com.generics;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
+/*
+ * Ensure the test data has Max Number in the First position
+ * Ensure the test data has Max Number in the Second position
+ * 
+ * */
 
 class MaximumNumTestCases {
-
+	//Tc1.1
 	@Test
-	void test() {
+	void testForFirstPosition() {
 		MaximumNum maximumNum = new MaximumNum();
-		Integer expectedOutput = maximumNum.findMaxNum(45, 23, 12);
-		Integer actualOutput = 45;
+		Integer expectedOutput = maximumNum.findMaxNum(4534, 783, 12);
+		Integer actualOutput = 4534;
+		assertEquals(expectedOutput, actualOutput);
+	}
+	
+	//Tc1.2
+	@Test
+	void testForSecondePosition() {
+		MaximumNum maximumNum = new MaximumNum();
+		Integer expectedOutput = maximumNum.findMaxNum(45, 783, 12);
+		Integer actualOutput = 783;
 		assertEquals(expectedOutput, actualOutput);
 	}
 }
